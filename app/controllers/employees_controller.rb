ef3be9398a358051b09d6785f2be4d1f17d56e4a
augustoibarrola <augusto.t.ibarrola@gmail.com>
2020-08-27 18:10:11 -0400

    completed all deliverables except dwight's rule + bonus deliverable

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -1,2 +1,32 @@
 class EmployeesController < ApplicationController
+    def index
+        @employee = Employee.all
+    end
+
+    def show 
+        @employee = Employee.find(params[:id])
+    end
+
+    def new 
+        @employee = Employee.new
+    end
+
+    def create
+        @employee = Employee.new(employee_params(:first_name, :last_name, :alias, :title, :office, :dog_id))
+        @employee.save
+        redirect_to employee_path(@employee)
+    end
+
+
+    def edit
+        @employee = Employee.find(params[:id])
+    end
+
+
+    private
+
+    def employee_params(*args)
+        params.require(:employee).permit(*args)
+    end
+
 end
